57786e9cf3887e84f06734103d5693ff251a1cb1
Snailsy5583 <r6awesome@gmail.com>
2021-09-19 08:09:04 -0500

    Add project files.

---
+++ src/Shader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+public:
+	Shader(const std::string & source);
+
+	int CreateShader();
+
+	ShaderProgramSource ParseShader();
+
+	static unsigned int CompileShader(unsigned int type, const std::string & source);
+
+private:
+	std::string& path;
+	ShaderProgramSource source;
+	unsigned int id;
+
+public:
+	inline ShaderProgramSource GetSource() const { return source; }
+	inline unsigned int GetID() const { return id; }
+};
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include "Shader.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+
+    unsigned int vbo;
+    float positions[] = {
+        -0.5f, -0.5f,
+         -0.5f,  0.5f,
+         0.5f, -0.5f
+    };
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    Shader basic("res/shaders/Basic.shader");
+    glUseProgram(basic.GetID());
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glDeleteProgram(basic.GetID());
+
+    glfwTerminate();
+    return 0;
+}
